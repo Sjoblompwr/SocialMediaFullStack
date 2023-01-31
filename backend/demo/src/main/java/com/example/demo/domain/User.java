3dbddc64a0aefd796066325c4a6301e449906506
David <davidsjoblom@hotmail.se>
2023-01-31 19:24:15 +0100

    meh

--- backend/demo/src/main/java/com/example/demo/domain/User.java
+++ backend/demo/src/main/java/com/example/demo/domain/User.java
@@ -2,13 +2,14 @@ package com.example.demo.domain;
 
 import java.util.List;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
-import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -17,6 +18,7 @@ import lombok.Setter;
 @NoArgsConstructor
 @Setter
 @Getter
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class User {
 
     public User(String username,String email, String password, String profileImageUrl, List<User> friends) {
