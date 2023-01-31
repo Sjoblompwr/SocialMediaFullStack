ad7dd7d83c89da7ebb5a0610e8a8ed00ee379771
Sjoblompwr <40866427+Sjoblompwr@users.noreply.github.com>
2023-01-31 14:18:47 +0100

    fixed with ignoreproperties friends

--- backend/demo/src/main/java/com/example/demo/domain/User.java
+++ backend/demo/src/main/java/com/example/demo/domain/User.java
@@ -1,10 +1,15 @@
 package com.example.demo.domain;
 
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -16,7 +21,7 @@ import lombok.Setter;
 @Getter
 public class User {
 
-    public User(String username,String email, String password, String profileImageUrl, User[] friends) {
+    public User(String username,String email, String password, String profileImageUrl, List<User> friends) {
         this.username = username;
         this.email = email;
         this.password = password;
@@ -31,6 +36,7 @@ public class User {
     private String email;
     private String password;
     private String profileImageUrl;
-    @OneToMany(fetch = FetchType.EAGER)
-    private User[] friends;
+    @ManyToMany
+    @JsonIgnoreProperties("friends")
+    private List<User> friends;
 }
